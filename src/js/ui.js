225709874202a9f4fd92fe60608e7fe54885d717
glorious73 <amjadmh73@gmail.com>
2020-07-18 22:00:19 +0300

    Showing modal now works with template

--- src/js/ui.js
+++ src/js/ui.js
@@ -3,25 +3,13 @@ export default class UI {
 
     }
 
-    showDeleteModal(e) {
-        // Prevent default
-        e = e || window.event;
-        e.preventDefault();
-        // Show modal
+    showGameFinishedModal() {
         const modal = document.getElementById("uiModal");
         const modalContent = document.getElementById("uiModalContent");
         modal.style.display = "block";
-        modalContent.innerHTML = `
-                    <div class="card card-color p-5 mt-4">
-                        <h2>Congrats!</h2>
-                        <hr />
-                        <h5 class="p-3">You finished the game in XYZ seconds.</h5>
-                        <hr />
-                        <div class="d-flex flex-row align-items-center justify-content-center">
-                            <button type="button" class="btn-play-again mx-2" onclick="playAgain()">Play Again</button>
-                        </div>
-                    </div>
-        `;
+        let template = document.getElementById('gameFinishedTemplate');
+        let fragment = document.importNode(template.content, true);
+        modalContent.appendChild(fragment);
     }
 
     closeUIModal() {
