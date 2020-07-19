f1339a6122829f291b4c058ffbb5096c7d57c68d
glorious73 <amjadmh73@gmail.com>
2020-07-19 11:03:44 +0300

    Dynamic game finished modal

--- src/js/ui.js
+++ src/js/ui.js
@@ -3,12 +3,25 @@ export default class UI {
 
     }
 
-    showGameFinishedModal() {
+    instantiateGameFinishedModal(timeElapsed) {
+        // grab template
+        let template = document.getElementById('gameFinishedTemplate');
+        let fragment = document.importNode(template.content, true);
+        // add event listeners
+        const button = fragment.querySelector('#playAgainButton');
+        button.addEventListener('click', this.reload);
+        // add time elapsed
+        fragment.querySelector('#gameTime').innerHTML = `You finished the game in ${timeElapsed} seconds.`;
+        return fragment;
+    }
+
+    showGameFinishedModal(timeElapsed) {
+        // Instantiate modal content
+        const fragment = this.instantiateGameFinishedModal(timeElapsed);
+        // Show modal
         const modal = document.getElementById("uiModal");
         const modalContent = document.getElementById("uiModalContent");
         modal.style.display = "block";
-        let template = document.getElementById('gameFinishedTemplate');
-        let fragment = document.importNode(template.content, true);
         modalContent.innerHTML = '';
         modalContent.appendChild(fragment);
     }
@@ -16,5 +29,9 @@ export default class UI {
     closeUIModal() {
         const modal = document.getElementById("uiModal");
         modal.style.display = "none";
+    }
+
+    reload() {
+        location.reload();
     }
 }
